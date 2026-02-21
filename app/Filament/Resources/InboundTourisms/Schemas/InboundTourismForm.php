2912bd4b649d59a3b992a20e88c82a941a793f70
jpineda <jpineda@senatur.gov.py>
2026-02-21 00:11:54 -0300

    Se agrega validacion para los datos unicos

--- app/Filament/Resources/InboundTourisms/Schemas/InboundTourismForm.php
+++ app/Filament/Resources/InboundTourisms/Schemas/InboundTourismForm.php
@@ -4,7 +4,9 @@ namespace App\Filament\Resources\InboundTourisms\Schemas;
 
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Utilities\Get;
 use Filament\Schemas\Schema;
+use Illuminate\Validation\Rule;
 
 class InboundTourismForm
 {
@@ -21,7 +23,7 @@ class InboundTourismForm
                     ->relationship('month', 'month')
                     ->required(),
                 Select::make('residence_country_id')
-                    ->relationship('country','name')
+                    ->relationship('country', 'name')
                     ->label('País de Residencia')
                     ->required(),
                 Select::make('entry_mode_id')
@@ -31,7 +33,27 @@ class InboundTourismForm
                 Select::make('travel_reason_id')
                     ->relationship('travelReason', 'description')
                     ->label('Motivo de viaje')
-                    ->required(),
+                    ->required()
+                    ->rules(function (Get $get, $record) {
+                        $rule = Rule::unique('inbound_tourisms', 'travel_reason_id')
+                            ->where(
+                                fn($q) => $q
+                                    ->where('year_id', $get('year_id'))
+                                    ->where('month_id', $get('month_id'))
+                                    ->where('residence_country_id', $get('residence_country_id'))
+                                    ->where('entry_mode_id', $get('entry_mode_id'))
+                                    ->where('travel_reason_id', $get('travel_reason_id'))
+                            );
+
+                        if ($record) {
+                            $rule->ignore($record->getKey());
+                        }
+
+                        return [$rule];
+                    })
+                    ->validationMessages([
+                        'unique' => 'Ya existe un registro con la misma combinación (Año, Mes, País, Vía de ingreso y Motivo).',
+                    ]),
                 TextInput::make('tourist_arrivals')
                     ->label('Llegadas Turistas')
                     ->required()

cfe8e06438e39651982adcc5012fa5cdd439802b
jpineda <jpineda@senatur.gov.py>
2026-02-23 23:52:33 -0300

    Se integra un widget

--- app/Filament/Resources/TourismProviderStats/Pages/ListTourismProviderStats.php
+++ app/Filament/Resources/TourismProviderStats/Pages/ListTourismProviderStats.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\TourismProviderStats\Pages;
 
 use App\Filament\Resources\TourismProviderStats\TourismProviderStatResource;
+use App\Filament\Widgets\TourismProviderStatsOverview;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
@@ -14,6 +15,13 @@ class ListTourismProviderStats extends ListRecords
     {
         return [
             CreateAction::make(),
+        ];
+    }
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            TourismProviderStatsOverview::class,
         ];
     }
 }
